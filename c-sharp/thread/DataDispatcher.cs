53f2c5f5f94e4ba3f6c85822383482eaaddf2423
robert <xf7577@126.com>
2020-08-23 21:51:59 +0800

    提交线程和线程池的简单封装基类

---
+++ c-sharp/thread/DataDispatcher.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace ChargeStub.Utils
+{
+    class DataDispatcher<T>
+    {
+        private ConcurrentQueue<T> packages = new ConcurrentQueue<T>();
+        private volatile bool exit = false;
+        private Thread thread;
+        private AutoResetEvent autoEvent = new AutoResetEvent(false);
+        private Action<T> action;
+
+        public DataDispatcher(Action<T> act)
+        {
+            action = act;
+            thread = new Thread(Run);
+        }
+        public void Add(T a)
+        {
+            packages.Enqueue(a);
+            autoEvent.Set();
+        }
+
+        public void Start()
+        {
+            exit = false;
+            if (thread.ThreadState == ThreadState.Unstarted
+                || thread.ThreadState == ThreadState.Stopped)
+            {
+                thread.Start();
+            }
+        }
+
+        public void Stop()
+        {
+            exit = true;
+            autoEvent.Set();
+            if (thread.ThreadState == ThreadState.Running)
+            {
+                thread.Join();
+            }
+        }
+
+        private void Run()
+        {
+            T rp;
+            while (!exit)
+            {
+                if (packages.TryDequeue(out rp))
+                {
+                    action(rp);
+                }
+                else
+                {
+                    autoEvent.WaitOne();
+                }
+            }
+        }
+    }
+}
---
+++ c-sharp/thread/DataDispatcherPool.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChargeStub.Utils
+{
+    abstract class DataDispatcherPool<T>
+    {
+        protected int concurrents;
+        private List<DataDispatcher<T> > pools = new List<DataDispatcher<T> >();
+        private Action<T> action;
+
+        protected abstract int CalcWhich(T t);
+        protected DataDispatcherPool(int amount, Action<T> act)
+        {
+            concurrents = amount;
+            for (int i = 0; i < concurrents; ++i)
+            {
+                pools.Add(null);
+            }
+            action = act;
+        }
+
+        public void Init()
+        {
+            lock (pools)
+            {
+                for (int i = 0; i < concurrents; ++i)
+                {
+                    DataDispatcher<T> rps = new DataDispatcher<T>(action);
+                    rps.Start();
+                    pools[i] = rps; 
+                }
+            }
+
+        }
+
+        public void Stop()
+        {
+            for (int i = 0; i < concurrents; ++i)
+            {
+                DataDispatcher<T> rps = pools[i];
+                if (rps != null)
+                {
+                    rps.Stop();
+                }
+                pools[i] = null;
+            }
+        }
+
+        public void Add(T a)
+        {
+            int which = CalcWhich(a);
+            which = Math.Abs(which);
+            which = which % concurrents;
+            DataDispatcher<T> rps = pools[which];
+            if(rps != null)
+            {
+                rps.Add(a);
+            }
+        }
+    }
+}
